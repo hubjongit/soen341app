2c1cbbb38b569ac00a9ca2188222702128907703
Daniel Baggs <danielwillbaggs@gmail.com>
2021-02-16 00:20:29 -0500

    #23 Setup env on local machine

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Our first custom page in Django!</h1>")
+    return HttpResponse("<h1>Our first custom page in Django!</h1>Daniel now has a working environment!")
