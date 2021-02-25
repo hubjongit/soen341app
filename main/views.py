710636459d49e8f7eaac008334bd1ecd39a3774a
Anusha-Reddy-Lattupally <78437552+Anusha-Reddy-Lattupally@users.noreply.github.com>
2021-02-26 00:43:18 +0530

    #23 Got a basic dev environment on my computer

--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Our first custom page in Django!</h1>Daniel now has a working environment!</br>Oliver now "
-                        "has a working environment!</br>Ahmad now has a working environment!")
+                        "has a working environment!</br>Ahmad now has a working environment!</br>Anusha now has a working environment!")
