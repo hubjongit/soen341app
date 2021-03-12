6e642b136245a242ebc4f5c9e1f24f14f9b86e9e
Jonathan Hubermann <jonathan.hubermann@mail.concordia.ca>
2021-03-12 18:13:08 -0500

    #72 routing protection on frontend React complete

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -5,15 +5,12 @@ class ProtectedRoute extends React.Component {
 
     componentWillReceiveProps(nextProps, nextContext) {
         if (this.props.isAuthenticated !== nextProps.isAuthenticated) {
-            console.log('protectedroute componentWillReceiveProps');
-            // this.props.isAuthenticated = nextProps.isAuthenticated;
             this.setState({isAuthenticated: nextProps.isAuthenticated});
         }
     }
 
     render() {
         const {isAuthenticated: isAuthenticated, redirect: redirect, ...props} = this.props;
-        console.log('protectedroute rendering');
         return (
             isAuthenticated ?
                 <Route
