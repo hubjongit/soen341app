4f4f3fdb864b0ddc5fec6004cc0b5ce4a1611e6b
Daniel Baggs <danielwillbaggs@gmail.com>
2021-04-14 17:06:20 -0400

    #90 finished the buttons for dismiss and delete.

--- frontend/src/components/ReportsAdmin.js
+++ frontend/src/components/ReportsAdmin.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Avatar} from "@material-ui/core";
 import '../App.css';
-
+import {getCookie} from "../GlobalFunctions";
+import ReactDOM from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 
 class ReportsAdmin extends React.Component {
@@ -11,6 +13,65 @@ class ReportsAdmin extends React.Component {
         this.dismissReport = this.dismissReport.bind(this);
     };
 
+    deletePost = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('id', this.props.id);
+
+        const csrftoken = getCookie('csrftoken')
+
+        fetch('/api/feed/', {
+            method: 'DELETE',
+            body: JSON.stringify(Object.fromEntries(formData)),
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRFToken': csrftoken,
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success === 'true') {
+                    return this.push('/report');
+                }
+                const ErrorsList = () => (
+                    <ul>{data.errors.map(error => <li key={error}> {error} </li>)}</ul>
+                );
+                const rootElement = document.getElementById("post-response-errors");
+                ReactDOM.render(<ErrorsList/>, rootElement);
+            })
+            .catch(error => console.log(error))
+    }
+
+
+    dismissReport = (id) => {
+        const formData = new FormData();
+        formData.append('id', id);
+
+        const csrftoken = getCookie('csrftoken')
+
+        fetch('/api/report/', {
+            method: 'DELETE',
+            body: JSON.stringify(Object.fromEntries(formData)),
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRFToken': csrftoken,
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success === 'true') {
+                    return this.props.history.push('/report');
+                }
+                const ErrorsList = () => (
+                    <ul>{data.errors.map(error => <li key={error}> {error} </li>)}</ul>
+                );
+                const rootElement = document.getElementById("post-response-errors");
+                ReactDOM.render(<ErrorsList/>, rootElement);
+            })
+            .catch(error => console.log(error))
+    }
+
+
     render() {
         return (
             <div className="report-card">
@@ -19,6 +80,8 @@ class ReportsAdmin extends React.Component {
 
                 <div className='report-content'>
                     <div className="report-header">
+                        <SubmitButton type='submit' text="Delete Post" classes='report-form-btn'
+                                      onClick={this.deletePost}/>
                     </div>
 
 
@@ -26,7 +89,7 @@ class ReportsAdmin extends React.Component {
                         {this.props.reports.map((reports) => {
                             return (
                                 <Report id={reports.id} report={reports.content} username={reports.username}
-                                        />
+                                        handleDismissReport={this.dismissReport}/>
                             )
                         })}
                     </div>
@@ -37,7 +100,7 @@ class ReportsAdmin extends React.Component {
     }
 }
 
-function Report({report, username,}) {
+function Report({report, username, id, handleDismissReport}) {
 
     return (
         <div className='report-box'>
@@ -49,6 +112,9 @@ function Report({report, username,}) {
             <div className='mr-auto'>
                 <p className='report-box-username'>{username}</p>
                 <p className='report-box-text'>Content: {report}</p>
+                <SubmitButton type='submit' text="Dismiss" classes='report-form-btn'
+                              onClick={() => handleDismissReport(id)}/>
+
             </div>
         </div>
     )
